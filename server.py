207377fdd69eec2cccb4f74009c2f61551e03485
hanss314 <24215006+hanss314@users.noreply.github.com>
2018-04-01 13:24:13 -0700

    Base sanic application

--- server.py
+++ server.py
@@ -1,41 +1,30 @@
-from http import HTTPStatus
-import http.server
-import socketserver
-import requests
-import shutil
-import sys
-import io
+from sanic import Sanic, response
+from sanic.exceptions import NotFound, FileNotFound
 
-PORT = 8000
 
-class Handler(http.server.SimpleHTTPRequestHandler):
-    def __init__(self, request, client_addr, server):
-#        print(self.translate_path(self.path))
-#        print(request)
+PORT = 8080
+app = Sanic()
 
-        super().__init__(request, client_addr, server)
 
-    def do_GET(self):
-        if self.path.startswith('/yt/'):
-            url = 'https://youtube.com/watch?v=' + self.path[4:]
-            r = requests.get(url)
-            print(r.status_code)
-            
-            encoded = r.text.encode(sys.getfilesystemencoding(), 'surrogateescape')
-            f = io.BytesIO()
-            f.write(encoded)
-            f.seek(0)
-            self.send_response(HTTPStatus.OK)
-            self.send_header("Content-type", "text/html; charset=%s" % sys.getfilesystemencoding())
-            self.send_header("Content-Length", str(len(encoded)))
-            self.end_headers()
-            #return f
+@app.exception(NotFound)
+@app.exception(FileNotFound)
+async def not_found(request, exception):
+    return response.redirect('/404.html')
 
-            shutil.copyfileobj(f, self.wfile)
-#self.wfile.write(r.text)
-            return
-        http.server.SimpleHTTPRequestHandler.do_GET(self)
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port", PORT)
-    httpd.serve_forever()
+@app.route("/yt/<video>", methods=["GET"])
+async def youtube(request, video):
+    url = 'https://youtube.com/watch?v=' + video
+    return response.redirect(url)
+
+
+@app.route("/", methods=["GET"])
+async def serve_file(request):
+    return await response.file('index.html')
+
+
+app.static('/', './')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=PORT)
